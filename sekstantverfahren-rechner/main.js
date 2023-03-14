fb8830f6d6c1ba76b56c6202a4bc306e6974a8c6
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2023-03-14 16:50:26 +0100

    use rpi as backend i guess

--- sekstantverfahren-rechner/main.js
+++ sekstantverfahren-rechner/main.js
@@ -1,23 +1,27 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-setupCounter(document.querySelector('#counter'))
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQJk-go_4QSKFTVa9MbXDyuGdZ0y5dKlw",
+  authDomain: "st-try-96e9c.firebaseapp.com",
+  projectId: "st-try-96e9c",
+  storageBucket: "st-try-96e9c.appspot.com",
+  messagingSenderId: "396159879159",
+  appId: "1:396159879159:web:9b32d5d13b2822474250f2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+document.getElementById("calculatebutton").addEventListener("click", () => {
+  var value =  document.getElementById("inputform").value
+  console.log(value)
+
+})
+
