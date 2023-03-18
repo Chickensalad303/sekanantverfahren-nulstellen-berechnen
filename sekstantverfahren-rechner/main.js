9e445bd78efa57d46fa4063d33f58e4f199b8e9c
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2023-03-18 09:06:28 +0100

    hello

--- sekstantverfahren-rechner/main.js
+++ sekstantverfahren-rechner/main.js
@@ -1,22 +1,6 @@
 import './style.css'
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCQJk-go_4QSKFTVa9MbXDyuGdZ0y5dKlw",
-  authDomain: "st-try-96e9c.firebaseapp.com",
-  projectId: "st-try-96e9c",
-  storageBucket: "st-try-96e9c.appspot.com",
-  messagingSenderId: "396159879159",
-  appId: "1:396159879159:web:9b32d5d13b2822474250f2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 
 document.getElementById("calculatebutton").addEventListener("click", () => {
