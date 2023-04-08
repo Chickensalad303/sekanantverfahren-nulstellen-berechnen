5c947bedf3f1643c62b925df4f02d16b81667c6e
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2023-04-08 23:08:50 +0300

    making slow progress... time to make the parser. Since i already wrote the actual calculator in python i guess i could create a system where the javascript interacts with python. Maybe websockets idk

---
+++ vite-project/src/App.svelte
@@ -0,0 +1,125 @@
+<script>
+  var value = ""
+  var x_minus_one
+  var x_n
+
+  var output
+
+    var inputformula = (e) =>{
+      value = e.target.value
+    }
+    var minusOne = (e) => {
+      x_minus_one = e.target.value
+    }
+    var n = (e) => {
+      x_n = e.target.value
+    }
+
+
+    var calculate = () => {
+      if (value != ""){
+        if (value.includes("^")){
+          var parsedValue = value.replaceAll("^", "**")
+          console.log(parsedValue)
+        }
+        else{
+          console.log(value)
+        }
+
+      }
+    }
+
+
+</script>
+
+<main>
+  <h1>hello there<br>bobo</h1>
+  <div class="header">
+    <p>Note: for x to the power of y use: x^y</p>
+  </div>
+
+  <div class="inputform">
+    <div>
+      <h1>X-1:</h1>
+      <input type="text" id="x_minus" on:input={minusOne}>
+    </div>
+    <div>
+      <h1>Xn:</h1>
+      <input type="text" id="x_minus" on:input={n}>
+    </div>
+    <div>
+      <h1>formula:</h1>
+      <input type="text" id="formula" on:input={inputformula}>
+    </div>
+  </div>
+
+  
+  <div class="calculateButton"><input type="button" value="calculate" on:click={calculate}></div>
+
+
+
+</main>
+
+<style>
+
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style> -->
