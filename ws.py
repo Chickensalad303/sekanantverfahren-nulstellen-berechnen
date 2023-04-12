1d9edce9a4d72e0ad94269c1a978229fcbc33bdc
Chickensalad303 <91982781+Chickensalad303@users.noreply.github.com>
2023-04-12 22:04:26 +0200

    test

--- ws.py
+++ ws.py
@@ -37,7 +37,7 @@ async def handler(ws):
 
 async def startServer():
     async with websockets.serve(handler, "", 8080):# inits server on port 8001. Everything recieved goes into the handler func
-        print(websockets.__name__)
+        print(websockets.local_adress)
         await asyncio.Future() # makes it run forever, cuz server needs to be running, not stopping
 
 
